705ccecf02be40f34b53e0ab74ed8c39e92a264e
michael <mikaelmaaher2017@gmail.com>
2022-05-26 05:54:54 +0200

    divofred-tutorials

--- client/src/WebsiteSections.js
+++ client/src/WebsiteSections.js
@@ -1,5 +1,4 @@
-import { Icon } from "@iconify/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
  
 import { faVirusCovid,faBuildingColumns} from '@fortawesome/free-solid-svg-icons'
 
@@ -8,27 +7,20 @@ export const ArrayofSections = [
   {
     text: "Hearts care",
     icon: (
-      <Icon className="icons"
-     icon="medical-icon:cardiology"
-      />
+      ''
     ),
   },
   {
     text: "Lungs section",
     icon: (
-      <Icon className="icons"
-        icon="medical-icon:respiratory"
-      />
+     ''
     ),
   },
-  { text: "Eyes care", icon: <Icon className="icons"  icon="medical-icon:ophthalmology" /> },
+  { text: "Eyes care", icon: '' },
   {
     text: "Mental Health",
     icon: (
-      <Icon  className="icons"
-        icon="medical-icon:mental-health"
-      />
-    ),
+     '')
   },
   { text: "Studies", icon: <FontAwesomeIcon className="icons"  icon={faBuildingColumns} />},
 ];
