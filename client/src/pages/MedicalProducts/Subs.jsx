1b1de1617995ddb6e7448269554f1ad400a62f34
michael <mikaelmaaher2017@gmail.com>
2022-06-24 21:43:09 +0200

    divofred-tutorials

--- client/src/pages/MedicalProducts/Subs.jsx
+++ client/src/pages/MedicalProducts/Subs.jsx
@@ -1,83 +1,91 @@
 import React from "react";
 import "./Subs.css";
 import Sub from "./Sub.png";
-import { Post,Get } from "../../utils/JsonOperations";
+import { Post, Get } from "../../utils/JsonOperations";
 import { useState } from "react";
 import { useEffect } from "react";
 function Subs() {
-  const [email,setEmail]=useState(null);
-  const [emails,setEmails]=useState([{
-    
-  }])
-  const [showsuccess,setShowsuccess]=useState(false);
-  useEffect(()=>{
-   
-   fetchGreeting()
-  
-  },[]) 
+  const [email, setEmail] = useState(null);
+  const [emails, setEmails] = useState([{}]);
+  const [showsuccess, setShowsuccess] = useState(false);
+  useEffect(() => {
+    fetchGreeting();
+  }, []);
   async function fetchGreeting() {
-      const response = await fetch('httpS://https://medicalprojectnet.herokuapp.com/graphql', {
-         method: 'POST',
-         headers: {
-           'content-type': 'application/json',
-          
-         },
-         body: JSON.stringify({
+    const response = await fetch(
+      "httpS://https://localhost:5000/graphql",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
           query: `
           query{
             get{
               email
             }
           } 
-       `
-         })
-      });
-      const responseBody = await response.json();
-      console.log(responseBody);
-     setEmails(responseBody.data.get);
-    }
-console.log(emails[0].email)
-  const sendData=async()=>{
- await Post('httpS://https://medicalprojectnet.herokuapp.com/api/prods/postemail',{email}).then((data)=>
- {
- console.log(data)
-if (data.email){
-setShowsuccess(true)
-setEmail('Done!')
-}
-else{
-setShowsuccess(false)
-setEmail("Enter Email Again!")
-}
- }
- )
-
-      
-
+       `,
+        }),
+      }
+    );
+    const responseBody = await response.json();
+    console.log(responseBody);
+    setEmails(responseBody.data.get);
   }
+  console.log(emails[0].email);
+  const sendData = async () => {
+    await Post(
+      "httpS://http://localhost:5000/api/prods/postemail",
+      { email }
+    ).then((data) => {
+      console.log(data);
+      if (data.email) {
+        setShowsuccess(true);
+        setEmail("Done!");
+      } else {
+        setShowsuccess(false);
+        setEmail("Enter Email Again!");
+      }
+    });
+  };
   return (
     <>
       <div className="ToContain">
         <div className="firstDiv">
-          <h2 className="MainTitle"> {showsuccess?'Congratulations🎉 Done ! ':'Keep in the know'}  </h2>
+          <h2 className="MainTitle">
+            {" "}
+            {showsuccess
+              ? "Congratulations🎉 Done ! "
+              : "Keep in the know"}{" "}
+          </h2>
           <p className="secondtitle">
-          {showsuccess?'You will be delivered by our newest Products.':'Subscribe to keep up-to-date with everything .'}  
+            {showsuccess
+              ? "You will be delivered by our newest Products."
+              : "Subscribe to keep up-to-date with everything ."}
           </p>
           <div className="SearchContainer">
             <input
-             disabled={showsuccess?true:false}
+              disabled={showsuccess ? true : false}
               type="email"
               placeholder="Enter your email..."
               className="inpt"
               value={email}
-              onChange={(e)=>setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
-         {emails[0].email}
-            <button  disabled={showsuccess?true:false} className="sub" onClick={(e)=>sendData()}> {showsuccess?'Subscribed':'Subscribe'}     </button>
+            {emails[0].email}
+            <button
+              disabled={showsuccess ? true : false}
+              className="sub"
+              onClick={(e) => sendData()}
+            >
+              {" "}
+              {showsuccess ? "Subscribed" : "Subscribe"}{" "}
+            </button>
           </div>
         </div>
         <div className="SecondDiv">
-
           <img className="imga" src={Sub} />
         </div>
       </div>
